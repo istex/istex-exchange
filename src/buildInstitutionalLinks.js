00f3d8d16301eefbd542dbf34cbb74479c35f3c1
parentin <jean-joffrey.parentin@inist.fr>
2021-04-20 15:30:19 +0200

    fix(holdings): Change holdings default base URL, make other_link_label silent

--- src/buildInstitutionalLinks.js
+++ src/buildInstitutionalLinks.js
@@ -28,7 +28,7 @@ const rootEndTag = '</institutional_links>';
 const institution = '<institution lang="en">ISTEX</institution>';
 const keywords = '<keywords>ISTEX France</keywords>';
 const electronicLinkLabel = '<electronic_link_label lang="en">[PDF] ISTEX</electronic_link_label>';
-const otherLinkLabel = '<other_link_label lang="en">[PDF] ISTEX</other_link_label>';
+const otherLinkLabel = '<other_link_label lang="en"/>';
 const openUrlBase = '<openurl_base>https://view.istex.fr/document/openurl?auth=ip,fede&amp;</openurl_base>';
 const openUrlOption = '<openurl_option>pmid</openurl_option>'
                       + '<openurl_option>doi</openurl_option>'
